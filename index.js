f3d196d20b0e3e0ab49e30bee3b91b3906312f8c
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-04-24 22:15:54 +0200

    add express server

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const pool = require('/db');
+
+// require dotenv
+// add dotenv for password
+
+// Routes
+
+// create a todo
+
+// get a todo
+
+// update a todo
+
+// delete a todo
+
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.listen(5001, () => {
+  console.log('server has started on port 5001')
+})
