4cc5708a9364a7886dbba32b0fc42ddcd21075f7
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-04-25 22:37:01 +0200

    add route to get one todo and update specific todo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-// const pool = require('/db');
+const pool = require('./db');
 
 // require dotenv
 // add dotenv for password
@@ -12,22 +12,61 @@ app.use(express.json()) // req.body
 
 // Routes
 
+// create a todo
 app.post('/todos', async (req, res) => {
   try {
-    console.log(req.body)
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
+
+    res.json(newTodo.rows[0])
   } catch (err) {
     console.error(err.message)
   }
 })
 
+// get all todos
+app.get('/todos', async(req, res) => {
+  try {
+    const allTodos = await pool.query('SELECT * FROM todo')
+    res.json(allTodos.rows)
+  } catch (err) {
+    console.error(err)
+  }
+})
 
-// create a todo
-
-// create a todo
 
 // get a todo
+app.get('/todos/:id', async(req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query('SELECT * FROM todo WHERE todo_id = $1', [
+      id
+    ])
+    
+    res.json(todo.rows[0])
+  } catch (error) {
+    console.error(error)
+  }
+})
 
 // update a todo
+app.put('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
+    
+    res.json('Todo was updated!');
+  } catch (error) {
+    console.error(error)
+  }
+})
 
 // delete a todo
 
