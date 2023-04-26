8b663871415bdf348ed9672e44cf4af3f55f7795
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-04-26 22:04:18 +0200

    clean up files and changr to jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
-import './App.css'
+import InputTodo from './components/InputTodo'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>hello</h1>
+      <InputTodo />
     </>
   )
 }
