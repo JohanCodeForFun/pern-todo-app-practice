c01c280f6ad176a4ceaacb8d54a5b27245434bf6
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-04-27 21:53:33 +0200

    setup ListTodos with a bootstrap 4 table

---
+++ client/src/components/ListTodos.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const ListTodos = () => {
+  return (
+    <>
+      <h1>List Todos</h1>;
+      <table className="table mt-5 text-center">
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/*
+          <tr>
+            <td>John</td>
+            <td>Doe</td>
+            <td>john@example.com</td>
+          </tr>
+          */}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default ListTodos;
